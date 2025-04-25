8c1ca532f9205fa7dde48313276a283204ea53ee
Spagum <ethanlee9113@gmail.com>
2025-04-25 11:52:56 +1000

    clerk implemtation

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+// app/page.tsx
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+
+      <SignedIn>
+        <h1 className="text-2xl font-bold">Welcome to your dashboard 🎉</h1>
+      </SignedIn>
     </>
   );
 }
